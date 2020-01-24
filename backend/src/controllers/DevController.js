91e457579a7f719dc1fb05ed2c68c9a2e93cb34b
Joao Martin <joao.martin@8x8.com>
2020-01-24 14:28:19 -0300

    first commit

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+// index, show, store, update, destroy
+
+module.exports = {
+    async index(request, response) {
+        const devs = await Dev.find();
+        return response.json(devs);
+    },
+
+    async destroy(request, response) {
+        const githubUsername = request.params;
+        await Dev.deleteMany({
+            github_username: githubUsername,
+        });
+    },
+
+    async store(request, response) {
+        const { github_username, techs, latitude, longitude } = request.body;
+
+        let dev = await Dev.findOne({ github_username });
+
+        if(!dev) {
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    
+            const {name = login, avatar_url, bio } = apiResponse.data;
+            const techsArray = parseStringAsArray(techs);
+        
+            const location = {
+                type: "Point",
+                coordinates: [longitude, latitude],
+            }
+        
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techsArray,
+                location,
+            });
+        
+            console.log(name, avatar_url, bio, github_username, techs);
+        }
+        return response.json(dev);
+    }
+};
